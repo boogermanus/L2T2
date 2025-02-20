44b1d2956a86ddd1a069ec542bff94007e2d1116
Jordan Woodruff <boogermanus@gmail.com>
2025-02-20 11:28:15 -0600

    more formatting and apiKey.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { L2T2Component } from "./components/l2-t2/l2-t2.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-
-  ],
+    L2T2Component
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/l2-t2/l2-t2.component.ts
+++ src/app/components/l2-t2/l2-t2.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-l2-t2',
   standalone: true,
-  imports: [],
+  imports: [
+
+  ],
   templateUrl: './l2-t2.component.html',
   styleUrl: './l2-t2.component.scss'
 })
